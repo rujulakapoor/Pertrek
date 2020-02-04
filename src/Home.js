a15ff178cbd028c49984c0c36a2d4ecbcf8652c5
Peter Jung <peterjung617@gmail.com>
2020-02-04 16:16:16 -0500

    Added dropdown

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import fire from './config/fire'
-
-
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 class Home extends Component {
     constructor(props) {
@@ -22,10 +22,29 @@ class Home extends Component {
         return (
           
            <body >
+           <Navbar bg="light" expand="lg" className="nav-bar">
+              <Navbar.Brand href="#home">Pertrek</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href=".">Home</Nav.Link>
+                  <Nav.Link href=".">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown" className="dropdown">
+                    <NavDropdown.Item href="#action/3.1" >Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2" >Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3" >Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form inline>
+                  <div id="left"><button onClick={this.logout}>Log in</button></div>
+                  <div id="left"><button onClick={this.onNavigateHome}>Log IIn</button></div>
+                </Form>
+              </Navbar.Collapse>
+            </Navbar>
+           <div id="three" className="col">
            
-           <div id="three" className="col-md-6">
-           <div id="left"><button onClick={this.logout}>Log in</button></div>
-           <div id="left"><button onClick={this.onNavigateHome}>Log IIn</button></div>
            <h3>P E R T R E K</h3>
            <h2>YOUR PERFECT VACATION STARTS HERE</h2>
            <div class="dropdown">
