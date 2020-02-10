9615be6e641d86251ecb35d6fa1a0afb0833d285
Peter Jung <peterjung617@gmail.com>
2020-02-10 00:17:58 -0500

    Tweaked navbar, added account page

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,18 +10,16 @@ class Navigation extends React.Component {
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
                 <Nav.Link href=".">Home</Nav.Link>
-                <Nav.Link href=".">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown" className="dropdown">
-                <NavDropdown.Item href="#action/3.1" >Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2" >Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3" >Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="./planner">Planner</Nav.Link>
             </Nav>
             <Form inline>
-                <div id="left"><button onClick={this.props.value}>Log in</button></div>
-                <div id="left"><button onClick={this.onNavigateHome}>Log IIn</button></div>
+                <NavDropdown title="Hello, ______" id="basic-nav-dropdown" className="dropdown">
+                  <NavDropdown.Item href="./account" >Account Details</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2" >Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3" >Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4" onClick={this.props.value}>Sign Out</NavDropdown.Item>
+                </NavDropdown>
             </Form>
             </Navbar.Collapse>
          </Navbar>
