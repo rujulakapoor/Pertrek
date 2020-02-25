23b5d9f39fadbe21ea8a36ec55501099e938f8b9
lauren <lauren.lum50@gmail.com>
2020-02-24 22:43:43 -0500

    cleaning up test files

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Planner from './Planner'
 import Account from './Account'
-import Yelp from './anotherYelpTest'
 import Scheduler from './Scheduler'
 
 const Main = () => (
@@ -12,7 +11,6 @@ const Main = () => (
             <Route exact path='/' component={Home}/>
             <Route path='/planner' component={Planner}/>
             <Route path='/account' component={Account}/>
-            <Route path='/yelp' component={Yelp}/>
             <Route path='/scheduler' component={Scheduler}/>
         </Switch>
     </main>
