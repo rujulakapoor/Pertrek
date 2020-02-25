0836470925d5c84a77fc5d8279626b9897b9700f
lauren <lauren.lum50@gmail.com>
2020-02-24 22:41:07 -0500

    home screen takes custom input but is v ugly :(

--- src/Home.js
+++ src/Home.js
@@ -15,6 +15,11 @@ class Home extends Component {
         super(props);
         this.logout = this.logout.bind(this);
         this.onNavigateHome = this.onNavigateHome.bind(this);
+
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         
     }
     logout() {
@@ -22,6 +27,14 @@ class Home extends Component {
     }
     onNavigateHome(){
       this.props.history.push('/Login');
+    }
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      //alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
     }
    
 
@@ -33,7 +46,7 @@ class Home extends Component {
               <h3>P E R T R E K</h3>
               <h2>YOUR PERFECT VACATION STARTS HERE</h2>
 
-              <div class="dropdown">
+              {/* <div class="dropdown">
 
                 <button class="dropbtn">Select A Destination</button>
                 <div class="dropdown-content">
@@ -56,9 +69,35 @@ class Home extends Component {
                   <a href="#">Cincinnati</a>
                 </div>
                 
-              </div>
+              </div>        */}
+
+
+              <Form onSubmit={this.handleSubmit}>
+                <label>
+                  <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <Link to={{
+                      pathname: "/scheduler/" + this.state.value,
+                    }}><input type="submit" value="Submit" /></Link>
+                
+              </Form>
+
+              {/* <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+
+                </Form.Group>
+
+                <Link to={{
+                      pathname: "/scheduler/" + this.state.value,
+                    }}><input type="submit" value="Submit" /></Link>
+
+              </Form> */}
 
             </div>
+
+            
           </div>
         );
       }
